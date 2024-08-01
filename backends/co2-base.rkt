505649e8e0227e14f88a2cd6e986b50749532701
William J. Bowman <wjb@williamjbowman.com>
2024-08-01 16:05:24 -0700

    Got initial draft of co2 model, and working for phi3 backend

--- backends/co2-base.rkt
+++ backends/co2-base.rkt
@@ -1,10 +1,44 @@
 #lang racket/base
 
+(require racket/match)
+
 (provide
- fprintf-co2-info
- make-co2-info
- displayln-co2-info)
+ (struct-out model-cost-info)
+ (struct-out model-energy-params)
+ model-cost-info->string
+ current-cost-port)
+
+(define current-cost-port (make-parameter (current-error-port)))
 
 (struct model-energy-params (alpha-k-s-0 alpha-k-s-1 alpha-k-s-2))
 
-(struct co2-info (model-params  input-tokens output-tokens training-co2))
+(struct model-cost-info (query-tco2/kwh model-params input-tokens output-tokens training-tco2 training-kwh))
+
+(define AVERAGE-ANNUAL-KWH-AMERICAN-HOUSE 10500)
+
+(define (model-cost-info->co2 info)
+ (define-values (kwh-query _) (model-cost-info->kwh info))
+ (values (* kwh-query (model-cost-info-query-tco2/kwh info)) (model-cost-info-training-tco2 info)))
+
+(define JOULES/KWH 2.77778e-7)
+
+(define (model-cost-info->kwh info)
+ (match-define
+  (model-cost-info _1 (model-energy-params alpha-k-s-0 alpha-k-s-1 alpha-k-s-2) input-tokens output-tokens _2 training-kwh)
+  info)
+ (define kwh-query
+  (* JOULES/KWH (+ (* alpha-k-s-0 input-tokens) (* alpha-k-s-1 output-tokens) (* alpha-k-s-2 input-tokens output-tokens))))
+ (values kwh-query training-kwh))
+
+(define (model-cost-info->string info)
+  (define-values (co2-query co2-training) (model-cost-info->co2 info))
+  (define-values (kwh-query kwh-training) (model-cost-info->kwh info))
+  (string-append
+   (format "This query cost ~a tCO2, with training cost of ~a tCO2~n" co2-query co2-training)
+   #;(let-values ([(co2-emitter co2-emission) (co2-order-of-magntitude co2-query)])
+   (format "For reference, they query is close to the CO2 emitted by ~a (~a tCO2)~n"
+    co2-emitter co2-emission))
+   (format "This query cost ~a KWh, with training cost of ~a KWh~n"
+    kwh-query kwh-training)
+   (format "For reference, this could power an average American house for ~a years~n"
+    (/ (+ kwh-query kwh-training) AVERAGE-ANNUAL-KWH-AMERICAN-HOUSE))))
--- backends/ollama-phi3.rkt
+++ backends/ollama-phi3.rkt
@@ -8,15 +8,18 @@
 
 ; https://github.com/ollama/ollama/blob/main/docs/api.md#generate-a-completion
 
-; estimated using LLMCarbon, using some guesses for energy efficiency
-(define phi3-training-co2
+; estimated using LLMCarbon, using data from Nvidia for hardware, and some guesses for energy efficiency and system power.
+(define phi3-training-tco2
  2.71062826343271)
 
-(define phi3-training-mwh
- 6.776570658581774)
+(define phi3-training-kwh
+ (* 1000 6.776570658581774))
 
 ; guesses based on similar models; using llama 2 7B
-(define phi3-model-params (model-energy-params (/ -3.89 2) (/ 31.52 2) (/ (* 4.27 (exp -2)) 2)
+(define phi3-model-params (model-energy-params (/ -3.89 2) (/ 31.52 2) (/ (* 4.27 (exp -2)) 2)))
+
+; https://ourworldindata.org/grapher/carbon-intensity-electricity
+(define tco2/kwh (* 170 1.102311e-6))
 
 (define (phi3-send-prompt! prompt)
   (define rsp
@@ -24,8 +27,10 @@
     #:json
     (hash 'model "phi3" 'prompt prompt 'stream #f)
     #:timeouts (make-timeout-config #:request (current-response-timeout))))
-  (displayln-co2-info
-    (make-co2-info (token-length prompt) (token-length response) phi3-training-co2))
-  (hash-ref (response-json rsp) 'response))
+  (define response (hash-ref (response-json rsp) 'response))
+  (displayln
+   (model-cost-info->string (model-cost-info tco2/kwh phi3-model-params (string-length prompt) (string-length response) phi3-training-tco2 phi3-training-kwh))
+   (current-cost-port))
+  response)
 
 (current-send-prompt! phi3-send-prompt!)
