4f91185a0a7d458e3cec61665d9d69c7f2edb574
William J. Bowman <wjb@williamjbowman.com>
2024-08-02 16:39:03 -0700

    Refactor cost models to support multiple inference cost models

--- backends/co2-base.rkt
+++ backends/co2-base.rkt
@@ -2,47 +2,43 @@
 
 (require
  racket/match
- racket/set)
+ racket/set
+ racket/generic)
 
 (provide
  (struct-out model-cost-info)
- (struct-out model-energy-params)
- model-cost-info->string
+ (struct-out inference-cost-info)
+ (struct-out wilkins-inference-model)
+ (struct-out time-avg-inference-model)
+ (struct-out cost-log-entry)
  current-cost-port)
 
-(struct model-energy-params (alpha-k-s-0 alpha-k-s-1 alpha-k-s-2))
+(struct inference-cost-info (input-tokens output-tokens prompt-duration response-duration))
 
-(struct model-cost-info (model-name query-tco2/kwh model-params input-tokens output-tokens training-tco2 training-kwh))
-
-(define AVERAGE-ANNUAL-KWH-AMERICAN-HOUSE 10500)
-
-(define (model-cost-info->co2 info)
- (define-values (kwh-query _) (model-cost-info->kwh info))
- (values (* kwh-query (model-cost-info-query-tco2/kwh info)) (model-cost-info-training-tco2 info)))
+(define-generics kwh-model
+ (model->kwh kwh-model info))
 
 (define JOULES/KWH 2.77778e-7)
 
-(define (model-cost-info->kwh info)
- (match-define
-  (model-cost-info _3 _1 (model-energy-params alpha-k-s-0 alpha-k-s-1 alpha-k-s-2) input-tokens output-tokens _2 training-kwh)
-  info)
- (define kwh-query
-  (* JOULES/KWH (+ (* alpha-k-s-0 input-tokens) (* alpha-k-s-1 output-tokens) (* alpha-k-s-2 input-tokens output-tokens))))
- (values kwh-query training-kwh))
+(struct wilkins-inference-model (alpha-k-s-0 alpha-k-s-1 alpha-k-s-2)
+ #:methods gen:kwh-model
+ [(define (model->kwh model info)
+   (match* (model info)
+    [((wilkins-inference-model alpha-k-s-0 alpha-k-s-1 alpha-k-s-2)
+      (inference-cost-info input-tokens output-tokens _1 _2))
+     (* JOULES/KWH (+ (* alpha-k-s-0 input-tokens) (* alpha-k-s-1 output-tokens) (* alpha-k-s-2 input-tokens output-tokens)))]))])
 
-(define (model-cost-info->string info)
-  (define-values (co2-query co2-training) (model-cost-info->co2 info))
-  (define-values (kwh-query kwh-training) (model-cost-info->kwh info))
-  (string-append
-   (format "This query cost ~a tCO2, with training cost of ~a tCO2~n" co2-query co2-training)
-   #;(let-values ([(co2-emitter co2-emission) (co2-order-of-magntitude co2-query)])
-   (format "For reference, they query is close to the CO2 emitted by ~a (~a tCO2)~n"
-    co2-emitter co2-emission))
-   (format "This query cost ~a KWh, with training cost of ~a KWh~n"
-    kwh-query kwh-training)
-   (format "For reference, this query could power an average American house for ~a years (and ~a years for the training costs)~n"
-    (/ kwh-query AVERAGE-ANNUAL-KWH-AMERICAN-HOUSE)
-    (/ kwh-training AVERAGE-ANNUAL-KWH-AMERICAN-HOUSE))))
+(define NANOSECONDS/HOURS (* 1000 1000 60 60))
+
+(struct time-avg-inference-model (kw)
+ #:methods gen:kwh-model
+ [(define (model->kwh model info)
+  (match* (model info)
+   [((time-avg-inference-model kw)
+     (inference-cost-info _1 _2 prompt-duration response-duration))
+    (* kw NANOSECONDS/HOURS (+ prompt-duration response-duration))]))])
+
+(struct model-cost-info (model-name query-tco2/kwh training-tco2 training-kwh inference-model))
 
 (provide
  string-stderr-model-cost-logger
@@ -50,6 +46,22 @@
  log->string
  current-model-cost-logger
  current-model-cost-log)
+
+;; TODO: Could reduce memory footprint of log by logging model-cost-info once, rather than per entry.
+;; Or using pair
+(struct cost-log-entry (model-cost-info inference-info))
+
+(define (cost-entry->co2 entry)
+ (match-define
+  (cost-log-entry (model-cost-info _1 query-tco2/kwh training-tco2 _2 inference-model) inference-info)
+  entry)
+ (values (* (model->kwh inference-model inference-info) query-tco2/kwh) training-tco2))
+
+(define (cost-entry->kwh entry)
+ (match-define
+  (cost-log-entry (model-cost-info _3 _1 _2 training-kwh inference-model) inference-info)
+  entry)
+ (values (model->kwh inference-model inference-info) training-kwh))
 
 (define current-model-cost-log (make-parameter '()))
 
@@ -62,10 +74,12 @@
 
 (define current-cost-port (make-parameter (current-error-port)))
 
-(define (log-model-cost! info)
- (let ([new-log (cons info (current-model-cost-log))])
+(define (log-model-cost! entry)
+ (let ([new-log (cons entry (current-model-cost-log))])
   (current-model-cost-log new-log)
   ((current-model-cost-logger) new-log)))
+
+(define AVERAGE-ANNUAL-KWH-AMERICAN-HOUSE 10500)
 
 (define (log->string log)
  (define-values (co2-query kwh-query co2-training-set kwh-training-set)
@@ -74,9 +88,9 @@
              ; probably should be hashes, mapping model names to costs
              [co2-training-set (set)]
              [kwh-training-set (set)])
-            ([info log])
-   (let-values ([(co2-query co2-training) (model-cost-info->co2 info)]
-                [(kwh-query kwh-training) (model-cost-info->kwh info)])
+            ([entry log])
+   (let-values ([(co2-query co2-training) (cost-entry->co2 entry)]
+                [(kwh-query kwh-training) (cost-entry->kwh entry)])
     (values (+ co2-query-cum co2-query) (+ kwh-query-cum kwh-query)
      (set-add co2-training-set co2-training)
      (set-add kwh-training-set kwh-training)))))
--- backends/ollama/phi3.rkt
+++ backends/ollama/phi3.rkt
@@ -1,8 +1,9 @@
 #lang racket/base
 
-(require 
+(require
  net/http-easy
  racket/port
+ racket/function
  "../config.rkt"
  "../co2-base.rkt")
 
@@ -17,10 +18,13 @@
 
 ; guesses based on similar models; using llama 2 7B
 ; Validation data: benchmarks suggest my iMac 27" 2017 3.5 i5 16 GB is drawing 1KW, which suggest the model is closer to 2x reality.
-(define phi3-model-params (model-energy-params (/ -3.89 2) (/ 31.52 2) (/ (* 4.27 (exp -2)) 2)))
+(define phi3-inference-model (wilkins-inference-model (/ -3.89 2) (/ 31.52 2) (/ (* 4.27 (exp -2)) 2)))
 
 ; https://ourworldindata.org/grapher/carbon-intensity-electricity
 (define tco2/kwh (* 170 1.102311e-6))
+
+(define phi3-cost-info
+ (model-cost-info 'ollama/phi3 tco2/kwh phi3-training-tco2 phi3-training-kwh phi3-inference-model))
 
 (define (phi3-send-prompt! prompt)
   (define rsp
@@ -28,10 +32,18 @@
     #:json
     (hash 'model "phi3" 'prompt prompt 'stream #f)
     #:timeouts (make-timeout-config #:request (current-response-timeout))))
+
   (define response-hash (response-json rsp))
-  (define response (hash-ref response-hash 'response))
+
   (log-model-cost!
-   (model-cost-info 'ollama/phi3 tco2/kwh phi3-model-params (hash-ref response-hash 'prompt_eval_count) (hash-ref response-hash 'eval_count) phi3-training-tco2 phi3-training-kwh))
-  response)
+   (cost-log-entry
+    phi3-cost-info
+    (inference-cost-info
+     (hash-ref response-hash 'prompt_eval_count)
+     (hash-ref response-hash 'eval_count)
+     (hash-ref response-hash 'prompt_eval_duration)
+     (hash-ref response-hash 'eval_duration))))
+
+  (hash-ref response-hash 'response))
 
 (current-send-prompt! phi3-send-prompt!)
