80dfdce92426dcc67ccd4449167b5060758b84cb
William J. Bowman <wjb@williamjbowman.com>
2024-08-02 13:15:22 -0700

    Improve changelog prompt

--- benchmarks/changelog-summary.rkt
+++ benchmarks/changelog-summary.rkt
@@ -12,10 +12,11 @@
 ; 10 minutes
 @(current-response-timeout (* 10 60))
 
-Please write a change log for all changes between versions 8.9 and the 8.10 for Racket.
-The complete commit history is given below:
+Please write a change log for release of the new v8.10 versions v8.9 and the v8.10 for Racket.
+The summary should include major changes since version v8.9, and be accessible to most Racket users and developers, informing them of major bug fixes, features, and feature changes.
+The complete git history for all commits between these two versions is given below:
 ```
-@(with-output-to-string (lambda () (system "cd racket; git --no-pager log")))
+@(with-output-to-string (lambda () (system "cd racket; git --no-pager log v8.9..v8.10")))
 ```
 
 @(displayln (prompt!))
