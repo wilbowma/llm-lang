b8f32ee46aa7d8bc8d202bdd0e474e9d8ed77708
William J. Bowman <wjb@williamjbowman.com>
2024-08-02 12:37:21 -0700

    Fix changelog benchmark
    It now runs. But it sucks

--- benchmarks/changelog-summary.rkt
+++ benchmarks/changelog-summary.rkt
@@ -1,15 +1,21 @@
 #lang llm-lang
 
-@(require llm-lang/backends/ollama-phi3 racket/file racket/system)
+@(require
+  llm-lang/backends/ollama-phi3
+  racket/file
+  racket/system
+  racket/port)
 
-@(define tmpd (make-temporary-directory))
-@(current-directory tmpd)
-@(void (system "git clone https://github.com/racket/racket.git"))
+@(current-directory "/tmp/")
+@(void (system "git clone https://github.com/racket/racket.git || true"))
+
+; 10 minutes
+@(current-response-timeout (* 10 60))
 
 Please write a change log for all changes between versions 8.9 and the 8.10 for Racket.
 The complete commit history is given below:
 ```
-@(system "cd racket; git --no-pager log")
+@(with-output-to-string (lambda () (system "cd racket; git --no-pager log")))
 ```
 
-@(delete-directory tmpd)
+@(displayln (prompt!))
