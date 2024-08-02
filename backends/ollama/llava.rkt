66bf38924f7186ec2195373c00c548f7666010b3
William J. Bowman <wjb@williamjbowman.com>
2024-08-02 16:57:54 -0700

    Update llava to use new cost model abstractions

--- backends/ollama/llava.rkt
+++ backends/ollama/llava.rkt
@@ -1,6 +1,6 @@
 #lang racket/base
 
-(require 
+(require
  net/http-easy
  racket/port
  "../config.rkt"
@@ -8,16 +8,15 @@
 
 ; https://github.com/ollama/ollama/blob/main/docs/api.md#generate-a-completion
 
-; estimated using LLMCarbon, using data from Nvidia for hardware, and some guesses for energy efficiency and system power.
+;; Currently lies
 (define llava-training-tco2
  2.71062826343271)
 
 (define llava-training-kwh
  (* 1000 6.776570658581774))
 
-; guesses based on similar models; using llama 2 7B
-; Validation data: benchmarks suggest my iMac 27" 2017 3.5 i5 16 GB is drawing 1KW, which suggest the model is closer to 2x reality.
-(define llava-model-params (model-energy-params (/ -3.89 2) (/ 31.52 2) (/ (* 4.27 (exp -2)) 2)))
+; llava reports inaccurate token counts, so we're using time average with a a wild guess about my local machine's wattage
+(define llava-inference-model (time-avg-inference-model (current-system-kw)))
 
 ; https://ourworldindata.org/grapher/carbon-intensity-electricity
 (define tco2/kwh (* 170 1.102311e-6))
@@ -25,8 +24,17 @@
 (define current-llava-images (make-parameter '()))
 
 (provide llava-add-image!)
+
 (define (llava-add-image! base64-data)
  (current-llava-images (cons base64-data (current-llava-images))))
+
+(define llava-cost-info
+ (model-cost-info
+  'ollama/llava
+  tco2/kwh
+  llava-training-tco2
+  llava-training-kwh
+  llava-inference-model))
 
 (define (llava-send-prompt! prompt)
   (define rsp
@@ -37,18 +45,16 @@
   (current-llava-images '())
 
   (define response-hash (response-json rsp))
-  (define response (hash-ref response-hash 'response))
 
   (log-model-cost!
-   (model-cost-info
-    'ollama/llava 
-    tco2/kwh 
-    llava-model-params 
-    (hash-ref response-hash 'prompt_eval_count) 
-    (hash-ref response-hash 'eval_count) 
-    llava-training-tco2 
-    llava-training-kwh))
+   (cost-log-entry
+    llava-cost-info
+    (inference-cost-info
+     (hash-ref response-hash 'prompt_eval_count)
+     (hash-ref response-hash 'eval_count)
+     (hash-ref response-hash 'prompt_eval_duration)
+     (hash-ref response-hash 'eval_duration))))
 
-  response)
+  (hash-ref response-hash 'response))
 
 (current-send-prompt! llava-send-prompt!)
