6e875983a01af60c9bc874705b29110e55449919
William J. Bowman <wjb@williamjbowman.com>
2024-08-02 17:17:14 -0700

    Fix alt-text benchmark to filter png files

--- benchmarks/image-alt.rkt
+++ benchmarks/image-alt.rkt
@@ -5,12 +5,14 @@
   racket/file
   net/base64
   racket/port
-  racket/runtime-path)
+  racket/runtime-path
+  racket/path)
 
 @(current-response-timeout (* 10 60))
 
 @(define-runtime-path image-dir "images")
 
-@(for ([file (in-directory image-dir)])
+@(for ([file (in-directory image-dir)]
+       #:when (path-has-extension? file ".png"))
   (llava-add-image! (bytes->string/utf-8 (base64-encode (port->bytes (open-input-file file) #:close? #t) "")))
   (displayln (prompt! "Please provide alt text for the following image.")))
