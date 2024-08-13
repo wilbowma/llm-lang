2e9c687dd96fc9c14c3994e0d3c752d13f7ab735
William J. Bowman <wjb@williamjbowman.com>
2024-08-13 13:37:55 -0700

    Add cost models to GPT3.5-turbo

--- backends/gpt3-5.rkt
+++ backends/gpt3-5.rkt
@@ -1,11 +1,33 @@
 #lang racket/base
 
-(require 
+(require
  net/http-easy
  racket/port
- "config.rkt")
+ "config.rkt"
+ "co2-base.rkt")
 
 (provide OPENAI_API_KEY)
+
+; Using LLMCarbon's number for GPT 3, which is validated, but is probably an underestimate of the training cost of 3.5-Turbo.
+(define gpt3-training-tco2
+ 553.3446700507615)
+
+(define gpt3-training-kwh
+ (* 1000 1289.8477157360408))
+
+; Using Llama-2 13b as estimate. The parameter count of GPT3.5 Turbo, isn't public, but it's guessed to be 20B.
+; But, it's also apparently heavily optimized. Better to underestimate, I guess?
+;
+; https://x.com/labenz/status/1654853390839472134/photo/1
+; https://github.com/llv22/gpt4_essay/blob/master/GPT-4-9.JPG
+; https://www.reddit.com/r/LocalLLaMA/comments/17lvquz/comment/k7hjc8b/
+(define gpt3-inference-model (wilkins-inference-model -6.79 56.01 (* 7.29 (exp -2))))
+
+; https://ourworldindata.org/grapher/carbon-intensity-electricity
+(define tco2/kwh (* 170 1.102311e-6))
+
+(define gpt3-cost-info
+ (model-cost-info 'gpt3.5-turbo tco2/kwh gpt3-training-tco2 gpt3-training-kwh gpt3-inference-model))
 
 (define OPENAI_API_KEY
   (make-parameter #f
@@ -23,6 +45,20 @@
     (hasheq 'model "gpt-3.5-turbo"
             'messages (list (hash 'role "user" 'content prompt 'stream #f)))
     #:timeouts (make-timeout-config #:request 120)))
-  (hash-ref (hash-ref (list-ref (hash-ref (response-json rsp) 'choices) 0) 'message) 'content))
+  #;(displayln (response-json rsp))
+
+  (define response-hash (response-json rsp))
+  (define usage (hash-ref response-hash 'usage))
+
+  (log-model-cost!
+   (cost-log-entry
+    gpt3-cost-info
+    (inference-cost-info
+     (hash-ref usage 'prompt_tokens)
+     (hash-ref usage 'completion_tokens)
+     #f
+     #f)))
+
+  (hash-ref (hash-ref (list-ref (hash-ref response-hash 'choices) 0) 'message) 'content))
 
 (current-send-prompt! gpt3-5-send-prompt!)
