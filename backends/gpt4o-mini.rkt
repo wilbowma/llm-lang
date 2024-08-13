b466b09524c51258edc7e5e656b01719d895fd4d
William J. Bowman <wjb@williamjbowman.com>
2024-08-13 16:31:30 -0700

    Add GPT4o-mini backend

---
+++ backends/gpt4o-mini.rkt
@@ -0,0 +1,73 @@
+#lang racket/base
+
+(require
+ net/http-easy
+ racket/port
+ "config.rkt"
+ "cost-base.rkt")
+
+(provide OPENAI_API_KEY)
+
+; Estimates of GPT-4 say 1.8 trillion parameters. Probably GPT4o-mini is an optimization of GPT 4, and its training cost is lower bounded by GPT 4.
+; Using LLMCarbon's number for GPT 3, which is validated, but is probably an underestimate of the training cost of 3.5-Turbo.
+; https://github.com/llv22/gpt4_essay/blob/master/GPT-4-9.JPG
+(define gpt4-training-tco2
+ 3530.575367647057)
+
+(define gpt4-training-kwh
+ (* 1000 8229.779411764703))
+
+; Continuing to use GPT 3-turbo. This is probably an overestimate, because the inference cost has been optimized for 4o-mini.
+;
+; Using Llama-2 13b as estimate. The parameter count of GPT3.5 Turbo, isn't public, but it's guessed to be 20B.
+; But, it's also apparently heavily optimized. Better to underestimate, I guess?
+;
+; https://x.com/labenz/status/1654853390839472134/photo/1
+; https://www.reddit.com/r/LocalLLaMA/comments/17lvquz/comment/k7hjc8b/
+;
+; This seems to line up with estimates of energy usage of GPT 3 from brown2020a, which estimated .004kwh usage per request.
+; I'm getting .002 for small examples, so if we assume some constant overhead and/or optimization, or the input tokens matter, this seems to be validated.
+(define gpt4-inference-model (wilkins-inference-model -6.79 56.01 (* 7.29 (exp -2))))
+
+; https://ourworldindata.org/grapher/carbon-intensity-electricity
+(define tco2/kwh (* 170 1.102311e-6))
+
+;; using US average of Microsoft's servers from li2023
+(define L/kWh .55)
+
+(define gpt4-cost-info
+ (model-cost-info 'gpt4o-mini tco2/kwh L/kWh L/kWh gpt4-training-tco2 gpt4-training-kwh gpt4-inference-model))
+
+(define OPENAI_API_KEY
+  (make-parameter #f
+    (lambda (e)
+      (unless (string? e)
+       (error "OPENAI_API_KEY looks invalid; should be a string"))
+      e)))
+
+(define (gpt4o-mini-send-prompt! prompt)
+  (define rsp
+   (post "https://api.openai.com/v1/chat/completions"
+    #:headers
+    (hasheq 'Authorization (format "Bearer ~a" (OPENAI_API_KEY)))
+    #:json
+    (hasheq 'model "gpt-4o-mini"
+            'messages (list (hash 'role "user" 'content prompt 'stream #f)))
+    #:timeouts (make-timeout-config #:request 120)))
+  #;(displayln (response-json rsp))
+
+  (define response-hash (response-json rsp))
+  (define usage (hash-ref response-hash 'usage))
+
+  (log-model-cost!
+   (cost-log-entry
+    gpt4-cost-info
+    (inference-cost-info
+     (hash-ref usage 'prompt_tokens)
+     (hash-ref usage 'completion_tokens)
+     #f
+     #f)))
+
+  (hash-ref (hash-ref (list-ref (hash-ref response-hash 'choices) 0) 'message) 'content))
+
+(current-send-prompt! gpt4o-mini-send-prompt!)
