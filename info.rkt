d7a4b99bb8debce8573fc1e22b3bb2ab6a81dea7
William J. Bowman <wjb@williamjbowman.com>
2024-08-20 16:32:59 -0700

    Add missing dependencies to info

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "llm-lang")
-(define deps '("base" "scribble-lib" "http-easy-lib"))
+(define deps '("base" "scribble-lib" "http-easy-lib" "raart" "markdown" "with-cache"))
 (define build-deps '("racket-doc" "rackunit-lib"))
 ;(define scribblings '(("scribblings/llm-lang.scrbl" ())))
 (define pkg-desc "A prototype LLM-first programming language.")
