efd2b6ab12e296fdd4535d48ac0c520540d4d528
William J. Bowman <wjb@williamjbowman.com>
2024-08-21 16:37:57 -0700

    Move define-by-prompt into standard library

--- llm-test/llm/lang/tests/round-sigfigs.rkt
+++ llm-test/llm/lang/tests/round-sigfigs.rkt
@@ -1,13 +1,13 @@
 #lang llm
 
-@(require "llm-macros.rkt" racket/function)
+@(require llm llm/define racket/function)
 @(require (for-syntax llm/openai/gpt4o-mini))
 
 @; I happen to know GPT4 believes these functions exists.
 @(define log10 (curryr log 10))
 @(define ceil ceiling)
 
-@define-by-prompt[round-to-n]{
+@define-by-prompt![round-to-n]{
 Define a Racket function `round-to-n` that rounds a given number to a given number of significant digits.
 }
 
