b0e67559e68edab5f1e90667352044566b348c2b
William J. Bowman <wjb@williamjbowman.com>
2024-08-21 17:28:21 -0700

    Add llm-test package

---
+++ llm-test/info.rkt
@@ -0,0 +1,7 @@
+#lang info
+(define collection 'multi)
+(define deps '("llm-lib" "base"))
+(define build-deps '())
+(define update-implies '("llm-lib"))
+(define pkg-desc "Tests for \"llm-lib\".")
+(define pkg-authors '(wilbowma))
