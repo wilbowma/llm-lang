af0ddb11a1800ffd20f545bf0b29123302036618
William J. Bowman <wjb@williamjbowman.com>
2024-08-21 18:33:28 -0700

    Enable specifying an output file for define-by-prompt
    This is really necessary so the user can modify the program.
    This has to be separate from the cached response, since that's cached the entire response, in FASL, and we need a user-modifiable file for the definition

--- llm-lib/llm/define.rkt
+++ llm-lib/llm/define.rkt
@@ -7,6 +7,7 @@
   racket/function
   racket/port
   with-cache
+  racket/file
   llm))
 
 (provide (for-syntax (all-from-out llm)) define-by-prompt!)
@@ -14,14 +15,23 @@
 (define-syntax (define-by-prompt! syn)
  (syntax-parse syn
   ; TODO: no reason in principle this should be a string literal.
-  [(_ name:id prompt:str)
-   (define prompt-str (string-append (syntax->datum #'prompt) "\nReturn only unformatted code without explanation. Do not use Markdown. Do not include the `#lang` line."))
+  [(_ name:id (~optional (~seq #:output-file file:str)) prompt:str ...)
+   (define prompt-str (string-append (apply string-append (map syntax->datum (attribute prompt))) "\nReturn only unformatted code without explanation. Do not use Markdown. Do not include the `#lang` line."))
    (define name-str (format "~a" (syntax->datum #'name)))
    #`(begin
       #,@(map
           (curry datum->syntax syn)
           (with-input-from-string
-	   (prompt! prompt-str)
+	   (if (attribute file)
+               (let ([file (syntax->datum #'file)])
+                 (if (file-exists? file)
+                     (file->string file)
+                     (let ([resp (prompt! prompt-str)])
+                       (with-output-to-file file
+                        (lambda () (display resp))
+                        #:mode 'text #:exists 'replace)
+                        resp)))
+               (prompt! prompt-str))
            (lambda ()
             (let loop ([defs '()])
              (let ([d (read)])
