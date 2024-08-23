74107b58b32edc190413a0752a0e19224fca4d32
William J. Bowman <wjb@williamjbowman.com>
2024-08-23 12:08:25 -0700

    Remove reliance on raart

--- llm-lib/info.rkt
+++ llm-lib/info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection 'multi)
-(define deps '("base" "scribble-lib" "http-easy-lib" "raart" "markdown" "with-cache" "at-exp-lib"))
+(define deps '("base" "scribble-lib" "http-easy-lib" "markdown" "with-cache" "at-exp-lib"))
 (define build-deps '("rackunit-lib"))
 (define pkg-desc "A prototype LLM-first programming language, with LLM API libraries.")
 (define version "0.2")
