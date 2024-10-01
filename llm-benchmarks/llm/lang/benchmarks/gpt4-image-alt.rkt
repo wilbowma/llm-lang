b98e8781345143ed358ec2af7e189e1918b55e59
William J. Bowman <wjb@williamjbowman.com>
2024-10-01 15:05:50 -0700

    Improve alt text example

--- llm-benchmarks/llm/lang/benchmarks/gpt4-image-alt.rkt
+++ llm-benchmarks/llm/lang/benchmarks/gpt4-image-alt.rkt
@@ -6,13 +6,19 @@
   net/base64
   racket/port
   racket/runtime-path
-  racket/path)
+  racket/path
+  racket/dict
+  pict
+  racket/pretty)
 
 @(current-response-timeout (* 10 60))
 
 @(define-runtime-path image-dir "images")
 
-@(for ([file (in-directory image-dir)]
-       #:when (path-has-extension? file ".png"))
-  (gpt4-add-image! 'png (bytes->string/utf-8 (base64-encode (port->bytes (open-input-file file) #:close? #t) "")))
-  (displayln (prompt! "Please provide alt text for the following image.")))
+@(unprompt
+  (pretty-print
+   (for/fold ([images '()])
+             ([file (in-directory image-dir)]
+              #:when (path-has-extension? file ".png"))
+     (gpt4-add-image! 'png (bytes->string/utf-8 (base64-encode (port->bytes (open-input-file file) #:close? #t) "")))
+     (dict-set images (bitmap file) (prompt! "Please provide alt text for the following image.")))))
