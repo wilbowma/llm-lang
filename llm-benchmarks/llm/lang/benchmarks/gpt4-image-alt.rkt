cec401f666551db91c844edf82e33a9f66b02783
William J. Bowman <wjb@williamjbowman.com>
2024-10-01 15:17:14 -0700

    Tweak alt text example to better use DrRacket

--- llm-benchmarks/llm/lang/benchmarks/gpt4-image-alt.rkt
+++ llm-benchmarks/llm/lang/benchmarks/gpt4-image-alt.rkt
@@ -8,17 +8,17 @@
   racket/runtime-path
   racket/path
   racket/dict
-  pict
-  racket/pretty)
+  racket/pretty
+  pict)
 
 @(current-response-timeout (* 10 60))
 
 @(define-runtime-path image-dir "images")
 
+@(current-print (lambda (x) (if (void? x) (void) (pretty-print x))))
 @(unprompt
-  (pretty-print
-   (for/fold ([images '()])
-             ([file (in-directory image-dir)]
-              #:when (path-has-extension? file ".png"))
-     (gpt4-add-image! 'png (bytes->string/utf-8 (base64-encode (port->bytes (open-input-file file) #:close? #t) "")))
-     (dict-set images (bitmap file) (prompt! "Please provide alt text for the following image.")))))
+  (for/fold ([images '()])
+            ([file (in-directory image-dir)]
+             #:when (path-has-extension? file ".png"))
+    (gpt4-add-image! 'png (bytes->string/utf-8 (base64-encode (port->bytes (open-input-file file) #:close? #t) "")))
+    (dict-set images (bitmap file) (prompt! "Please provide alt text for the following image."))))
